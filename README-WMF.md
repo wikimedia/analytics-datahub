c4b1928dc491e09509a490c2919487249cc48649
Ben Tullis <btullis@wikimedia.org>
2022-08-26 11:16:14 +0100

    Build containers for version 0.8.43 of DataHub
    This will trigger the building of the datahub deployment pipeline.
    
    Update jetty for CVE
    
    This is as per the upstream fix.
    https://github.com/datahub-project/datahub/pull/4857
    
    Bug: T316336
    Change-Id: I338521f186ce2849e8627123ff510113a50dd4c5

--- README-WMF.md
+++ README-WMF.md
@@ -5,3 +5,5 @@ We maintain our modifications from the upstream in a branch named `wmf`.
 See this page for more details about the Upgrade process: https://wikitech.wikimedia.org/wiki/Analytics/Systems/DataHub/Upgrading
 
 * Released version 0.8.38
+
+* Released version 0.8.43
--- docker/datahub-gms/setup_gms.sh
+++ docker/datahub-gms/setup_gms.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-curl https://archiva.wikimedia.org/repository/mirror-maven-central/org/eclipse/jetty/jetty-runner/9.4.20.v20190813/jetty-runner-9.4.20.v20190813.jar --output jetty-runner.jar
-curl https://archiva.wikimedia.org/repository/mirror-maven-central/org/eclipse/jetty/jetty-jmx/9.4.20.v20190813/jetty-jmx-9.4.20.v20190813.jar --output jetty-jmx.jar
-curl https://archiva.wikimedia.org/repository/mirror-maven-central/org/eclipse/jetty/jetty-util/9.4.20.v20190813/jetty-util-9.4.20.v20190813.jar --output jetty-util.jar
+curl https://archiva.wikimedia.org/repository/mirror-maven-central/org/eclipse/jetty/jetty-runner/9.4.46.v20220331/jetty-runner-9.4.46.v20220331.jar --output jetty-runner.jar
+curl https://archiva.wikimedia.org/repository/mirror-maven-central/org/eclipse/jetty/jetty-jmx/9.4.46.v20220331/jetty-jmx-9.4.46.v20220331.jar --output jetty-jmx.jar
+curl https://archiva.wikimedia.org/repository/mirror-maven-central/org/eclipse/jetty/jetty-util/9.4.46.v20220331/jetty-util-9.4.46.v20220331.jar --output jetty-util.jar
 curl http://archiva.wikimedia.org/repository/mirror-maven-central/io/opentelemetry/javaagent/instrumentation/opentelemetry-javaagent/0.14.0/opentelemetry-javaagent-0.14.0.jar --output opentelemetry-javaagent-all.jar
 curl http://archiva.wikimedia.org/repository/mirror-maven-central/io/prometheus/jmx/jmx_prometheus_javaagent/0.16.1/jmx_prometheus_javaagent-0.16.1.jar --output jmx_prometheus_javaagent.jar
 curl -L https://github.com/treff7es/dockerize/releases/download/v0.6.1/dockerize-linux-amd64-v0.6.1.tar.gz | tar -C ./ -xzv
