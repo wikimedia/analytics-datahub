d406f50aaae5e46f23b1e62f945bc542737e49b5
Ben Tullis <btullis@wikimedia.org>
2023-06-29 16:51:33 +0100

    Update the kafka-setup conainer of datahub
    We have previously not used the kafka-setup container, but had setup
    kafka by-hand for our instances. Now we would like to start using it
    because it is the mechanism that upstream uses to introduce changes to
    the kafka topic handling in Datahub.
    
    This commit updates our image to bring it more into line with the
    upstream version. Specifically, it copies in two more scripts to the
    container and changes a few of the environment variables relating top
    topic names.
    
    Bug: T329514
    Change-Id: I21abbea3637b630b1baaca492f84fc9e3bd09bb7

--- docker/kafka-setup/setup_kafka_setup.sh
+++ docker/kafka-setup/setup_kafka_setup.sh
@@ -4,4 +4,4 @@ set -e
 mkdir ~/bin
 curl -L https://raw.githubusercontent.com/confluentinc/confluent-docker-utils/master/confluent/docker_utils/cub.py -o ./cub.py
 curl -L https://github.com/treff7es/dockerize/releases/download/v0.6.1/dockerize-linux-amd64-v0.6.1.tar.gz | tar -C ./ -xzv
-chmod +x /datahub/cub.py /datahub/kafka-setup.sh
+chmod +x /datahub/cub.py /datahub/kafka-setup.sh /datahub/kafka-config.sh /datahub/kafka-topic-workers.sh
