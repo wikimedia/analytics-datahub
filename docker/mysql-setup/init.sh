046939ce79f35e13083ea87d7d6bd1341ceef328
Ben Tullis <btullis@wikimedia.org>
2023-06-29 12:00:25 +0100

    Fix the path to the init.sql file in the mysql-setup container
    Unlike the upstream image, we do not store files on the root directory
    because of the way that blubber works. For that reason, we need to
    modify the location of the init.sql file that is referred to be sed when
    initialising the database.
    
    Bug: T329514
    Change-Id: I1f57b29e70877452989cc0699d9900678b27b3bc

--- docker/mysql-setup/init.sh
+++ docker/mysql-setup/init.sh
@@ -3,5 +3,5 @@
 : ${MYSQL_PORT:=3306}
 
 /datahub/dockerize -wait tcp://$MYSQL_HOST:$MYSQL_PORT -timeout 240s
-sed -e "s/DATAHUB_DB_NAME/${DATAHUB_DB_NAME}/g" /init.sql | tee -a /tmp/init-final.sql
+sed -e "s/DATAHUB_DB_NAME/${DATAHUB_DB_NAME}/g" /datahub/init.sql | tee -a /tmp/init-final.sql
 mysql -u $MYSQL_USERNAME -p"$MYSQL_PASSWORD" -h $MYSQL_HOST -P $MYSQL_PORT < /tmp/init-final.sql
